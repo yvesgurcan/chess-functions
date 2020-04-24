dbd94fba326d3750f602d78d2919c80e4d8bfa3c
yvesgurcan <gurcan.yves+github@gmail.com>
2020-04-23 22:27:32 -0700

    dotenv

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,26 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+require('dotenv').config();
 
 const getEnvironmentVariables = function (variableNames) {
     let values = {};
-    try {
-        const environmentVariables = JSON.parse(
-            fs.readFileSync(path.resolve('env.json'))
-        );
-        for (let i = 0; i < variableNames.length; i++) {
-            const variableName = variableNames[i];
-            values[variableName] = environmentVariables[variableName];
-        }
-    } catch (error) {
-        console.log(error);
-        for (let i = 0; i < variableNames.length; i++) {
-            const variableName = variableNames[i];
-            values[variableName] = process.env[variableName];
-        }
-    }
-
     for (let i = 0; i < variableNames.length; i++) {
         const variableName = variableNames[i];
+        values[variableName] = process.env[variableName];
         if (values[variableName] === undefined) {
             console.error(
                 `Environment variable '${variableName}' not defined.`
